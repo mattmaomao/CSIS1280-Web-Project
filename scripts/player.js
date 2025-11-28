29009928923b05fb129dc8a017f3356748f886d9
stacy29496 <stacy29496@gmail.com>
2025-11-27 20:35:46 -0800

    updated Players

--- scripts/player.js
+++ scripts/player.js
@@ -8,20 +8,26 @@ function hideAll() {
 
 function showPlayer(id) {
     hideAll();
+    let disapperPlaceHolder=document.getElementById("placeholder");
+    disapperPlaceHolder.style.display="none";
     let showPlayer = document.getElementById(id);
     if (showPlayer) {
         showPlayer.style.display = "block";
     }
 }
-
 hideAll();
-document.getElementById("btnMJ").addEventListener("click", function () {
-    showPlayer("MJ");
-});
 
-document.getElementById("btnKB").addEventListener("click", function () {
-    showPlayer("KB");
-});
+let btnMJ=document.getElementById("btnMJ");
+btnMJ.addEventListener("click", showPlayerMJ);
+function showPlayerMJ(){
+    showPlayer("MJ");
+}
+
+let btnKB=document.getElementById("btnKB");
+btnKB.addEventListener("click", showPlayerKB);
+function showPlayerKB(){
+    showPlayer("KB")
+}
 
 document.getElementById("btnMagic").addEventListener("click", function () {
     showPlayer("Magic");
@@ -59,3 +65,37 @@ document.getElementById("btnNJ").addEventListener("click", function () {
 document.getElementById("btnLD").addEventListener("click", function () {
     showPlayer("LD");
 });
+
+const players = [
+    { name: "Michael Jordan", image: "../images/MJ.png",section: "MJ" },
+    { name: "Kobe Bryant", image: "../images/KB.jpg",section: "KB"},
+    { name: "Magic Johnson", image: "../images/Magic.jpg",section:"Magic" },
+    { name: "Larry Bird", image: "../images/LB.jpg",section:"LB" },
+    { name: "Tim Duncan", image: "../images/TD.jpg" ,section:"TD"},
+    { name: "Dwyane Wade", image: "../images/DW.png",section:"DW" },
+
+    { name: "LeBron James", image: "../images/LJ.jpg",section:"LJ" },
+    { name: "Stephen Curry", image: "../images/SC.jpg",section:"SC" },
+    { name: "Giannis Antetokounmpo", image: "../images/GA.jpeg" ,section:"GA"},
+    { name: "Nikola Jokić", image: "../images/NJ.jpg" ,section:"NJ"},
+    { name: "Luka Dončić", image: "../images/LD.jpg",section:"LD" }
+];
+
+let btnRandom = document.getElementById("btnrandom");
+let randomPic = document.getElementById("randomPic");
+let randomCaption = document.getElementById("randomCaption");
+
+
+function getRandomPlayer(playerArray) {
+    return playerArray[Math.floor(Math.random() * playerArray.length)];
+}
+
+btnRandom.addEventListener("click", DisplayRandomPlayer);
+    
+function DisplayRandomPlayer() {
+    let picked = getRandomPlayer(players);
+    randomPic.src = picked.image;
+    randomCaption.textContent = picked.name;
+
+    showPlayer(picked.section);
+}
